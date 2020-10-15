80d66dca404f891c0e90f08293ddf56afa9e2dab
Guanmuyang <1321879427@qq.com>
2020-10-15 16:13:18 +0800

    first commit

---
+++ src/main/java/com/example/HaiKangDemo/HaiKangDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HaiKangDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiKangDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiKangDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HaiKangDemo/HaiKangDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HaiKangDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaiKangDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
